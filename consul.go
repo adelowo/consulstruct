5dcc674384e314f8dfe9f88477e81b1a68013f1c
Lanre Adelowo <me@lanreadelowo.com>
2018-03-15 16:15:49 +0100

    Add support for int,strings and booleans

--- consul.go
+++ consul.go
@@ -3,6 +3,8 @@ package consulstruct
 import (
 	"errors"
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/hashicorp/consul/api"
 )
@@ -20,7 +22,7 @@ func New(c *Config) *Decoder {
 
 type Config struct {
 	Prefix    string
-	store     *api.KV
+	Store     *api.KV
 	QueryOpts *api.QueryOptions
 }
 
@@ -39,10 +41,85 @@ func (d *Decoder) Decode(v interface{}) error {
 		return ErrNotStruct
 	}
 
-	_, _, err := d.config.store.List(d.config.Prefix, d.config.QueryOpts)
+	pairs, _, err := d.config.Store.List(d.config.Prefix, d.config.QueryOpts)
 	if err != nil {
 		return err
 	}
 
+	return d.decode(pointer.Elem(), pairsToDict(pairs))
+}
+
+func (d *Decoder) decode(ref reflect.Value, pairs map[string]*api.KVPair) error {
+
+	for i := 0; i < ref.Type().NumField(); i++ {
+		structField := ref.Type().Field(i)
+		val := fetch(ref.Type().Field(i))
+
+		if len(strings.TrimSpace(val)) == 0 {
+			continue
+		}
+
+		pair, ok := pairs[val]
+		if !ok {
+			continue
+		}
+
+		if err := set(ref.Field(i), structField, pair); err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func pairsToDict(pairs api.KVPairs) map[string]*api.KVPair {
+	var p = make(map[string]*api.KVPair, len(pairs))
+
+	for _, pair := range pairs {
+		p[pair.Key] = pair
+	}
+
+	return p
+}
+
+func fetch(val reflect.StructField) string {
+	return val.Tag.Get("consul")
+}
+
+// strings, ints and booleans
+func set(field reflect.Value, refType reflect.StructField, pair *api.KVPair) error {
+	var err error
+
+	s := string(pair.Value)
+
+	switch field.Kind() {
+
+	case reflect.String:
+		field.SetString(s)
+		break
+
+	case reflect.Int:
+		n, err := strconv.ParseInt(s, 10, 32)
+		if err != nil {
+			return err
+		}
+
+		field.SetInt(n)
+		break
+
+	case reflect.Bool:
+		truthy, err := strconv.ParseBool(s)
+		if err != nil {
+			return err
+		}
+
+		field.SetBool(truthy)
+		break
+
+	default:
+
+		err = errors.New("consulstruct : unsupported operation")
+	}
+
+	return err
 }
